0298af171fba55b1d386b6688202c9e4a55eb29c
Ali <alihasnain173@gmail.com>
2024-01-31 11:46:08 +0530

    bug fixes security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.plugins.push(
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                "window.jQuery": "jquery",
-            }));
-        return config;
-    }
-}
+  reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self';script-src 'self' https://apis.google.com https://ajax.googleapis.com 'unsafe-inline' 'unsafe-eval';  style-src 'self' 'unsafe-inline'",
+          }, // Customize your policy
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
+          { key: "Permissions-Policy", value: "geolocation=(), microphone=()" }, // Customize as needed
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true, // Set to false if it's not a permanent redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
